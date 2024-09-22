17e905f82f77b5a95ff5a257c8c6b5da18b76f03
HernandoNJ <hernandonj.unity@gmail.com>
2024-09-22 01:17:28 -0500

    HN - progress in Relay config

---
+++ Assets/Scripts/RelayManager.cs
@@ -0,0 +1,61 @@
+using System.Threading.Tasks;
+using TMPro;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+public class RelayManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI joinCodeText;
+    [SerializeField] private TMP_InputField joinCodeInputField;
+
+    private void Start()
+    {
+        UnityMultiplayerServicesInit();
+    }
+
+    private async void UnityMultiplayerServicesInit()
+    {
+        // add the await keyword to avoid game freezing
+        // because it takes some time to receive the response
+        // Initialize Unity Services
+        await UnityServices.InitializeAsync();
+
+        // Anonymous authentication
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async void StartRelay()
+    {
+        
+    }
+
+    // Async functions because it takes some time for them to be executed
+    // Make the async function return a string with Task<string>
+    public async Task<string> StartHostWithRelay(int maxConnections = 3)
+    {
+        // Create allocation in Relay setting the max number of players
+        var allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        
+        // Set Relay Server data
+        // Create new allocation and set the data transport protocol to dtls
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+
+        // Get join code if Host started successfully or null if not.
+        var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        return NetworkManager.Singleton.StartHost() 
+            ? joinCode 
+            : null;
+    }
+
+    // Check if the client connection is successful
+    public async Task<bool> void StartClientWithRelay(string joinCode)
+    {
+        
+    }
+}
