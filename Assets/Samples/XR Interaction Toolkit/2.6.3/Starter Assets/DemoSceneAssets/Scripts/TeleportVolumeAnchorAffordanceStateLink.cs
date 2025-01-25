0b222f76743d39d03e24b74c4a1d3c00228a78a3
CarlosSolis <kxr10s1@gmail.com>
2025-01-25 11:08:38 -0500

    reimported xrit sample assets

--- Assets/Samples/XR Interaction Toolkit/2.6.3/Starter Assets/DemoSceneAssets/Scripts/TeleportVolumeAnchorAffordanceStateLink.cs
+++ Assets/Samples/XR Interaction Toolkit/2.6.3/Starter Assets/DemoSceneAssets/Scripts/TeleportVolumeAnchorAffordanceStateLink.cs
@@ -26,7 +26,7 @@ namespace UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets
         }
 
         XRInteractableAffordanceStateProvider m_AffordanceStateProvider;
-        UnityEngine.XR.Interaction.Toolkit.Interactables.IXRInteractable m_Interactable;
+        IXRInteractable m_Interactable;
 
         /// <summary>
         /// See <see cref="MonoBehaviour"/>.
@@ -49,13 +49,13 @@ namespace UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets
             }
 
             var interactableSource = m_AffordanceStateProvider.interactableSource;
-            m_Interactable = interactableSource != null && interactableSource is UnityEngine.XR.Interaction.Toolkit.Interactables.IXRInteractable interactable
+            m_Interactable = interactableSource != null && interactableSource is IXRInteractable interactable
                     ? interactable
-                    : m_AffordanceStateProvider.GetComponentInParent<UnityEngine.XR.Interaction.Toolkit.Interactables.IXRInteractable>();
+                    : m_AffordanceStateProvider.GetComponentInParent<IXRInteractable>();
 
             if (m_Interactable == null)
             {
-                Debug.LogError($"Interactable source must be an {nameof(UnityEngine.XR.Interaction.Toolkit.Interactables.IXRInteractable)}.", this);
+                Debug.LogError($"Interactable source must be an {nameof(IXRInteractable)}.", this);
                 enabled = false;
                 return;
             }
