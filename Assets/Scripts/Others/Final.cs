4731a2dc8d9db441cebfd4dd263ed2d6cec5b122
CarlosSolis <kxr10s1@gmail.com>
2025-02-24 10:02:48 -0500

    Added new enemy and weapon

--- Assets/Scripts/Others/Final.cs
+++ Assets/Scripts/Others/Final.cs
@@ -5,10 +5,13 @@ namespace Others
 {
     public class Final : MonoBehaviour
     {
+        public ParticleSystem particle;
+        
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
+                particle.Play();
                 SoundManager.Instance.PlaySoundWin();
                 UIManager.Instance.DeactivateHud();
                 GameManager.Instance.locomotionSystem.SetActive(false);
