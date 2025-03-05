4837411c3376ec89446990d8e7b73c96fea7c8cb
CarlosSolis <kxr10s1@gmail.com>
2025-03-04 23:11:14 -0500

    Added new UI part1

---
+++ Assets/Scripts/Others/Info.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Others
+{
+    public class Info : MonoBehaviour
+    {
+        public void DestroyInfo(GameObject info)
+        {
+            Destroy(info);
+        }
+    }
+}
