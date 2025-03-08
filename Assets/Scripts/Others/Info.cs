b308050167439c118024f45889d325638a34db63
CarlosSolis <kxr10s1@gmail.com>
2025-03-08 18:59:49 -0500

    Added new UI in game room

--- Assets/Scripts/Others/Info.cs
+++ Assets/Scripts/Others/Info.cs
@@ -4,9 +4,17 @@ namespace Others
 {
     public class Info : MonoBehaviour
     {
+        public GameObject activateInfo;
+        
         public void DestroyInfo(GameObject info)
         {
             Destroy(info);
+        }
+
+        public void DestroyInfoAndActivateInfo(GameObject info)
+        {
+            Destroy(info);
+            activateInfo.SetActive(true);
         }
     }
 }
